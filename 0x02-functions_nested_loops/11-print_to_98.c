dd280e8a8c82cc48a4e3328fccccdb3688ab9710
0Soufiane <ess2003831@gmail.com>
2023-08-15 16:47:07 -0700

    hard

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,76 +1,35 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-* print_to_98 - To check  sign of number
-* @n: n is the number to start count from
-* Return: void (Sucess)
-*/
-
+ * print_to_98 -> 98 print to 98
+ * @n: number input
+ */
 
 void print_to_98(int n)
 {
-
-if (n > 98)
-{
-	while (n > 97)
+	if (n < 98)
 	{
-		write_num(n);
-		if (n != 98)
+		while (n <= 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			n++;
 		}
-		n--;
 	}
-}
-else
-{
-	while (n < 99)
+	else if (n > 98)
 	{
-		if (n < 0)
+		while (n >= 98)
 		{
-			int i = -1 * n;
-
-			_putchar('-');
-			write_num(i);
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			n--;
 		}
-		else
-		{
-			write_num(n);
-		}
-		if (n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n++;
-	}
-}
-_putchar(10);
-}
-
-/**
-* write_num - to write out number
-* @i: number to print
-* Return: void (Sucess)
-*/
-
-void write_num(int i)
-{
-	if (i > 9 && i < 100)
-	{
-		_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
-	}
-	else if (i > 99)
-	{
-		_putchar(i / 100 + '0');
-		_putchar((i / 10) % 10 + '0');
-		_putchar(i % 10 + '0');
 	}
 	else
-	{
-		_putchar(i + '0');
-	}
-}
+		printf("98");
+	printf("\n");
+}}
