43f0e80f8f7d6365830b2c74158a7196617bb27f
0Soufiane <ess2003831@gmail.com>
2023-08-24 15:44:06 -0700

    printnumber

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,38 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
-
+ * print_number - print number
+ * @n: number to print
+ */
 void print_number(int n)
 {
-unsigned int n1;
+	unsigned int tens, unit, positive_value = n;
+	double tens_value = 1;
 
-n1 = n;
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			positive_value = (n * -1);
+			_putchar('-');
+		}
+		while (tens_value <= positive_value)
+		{
+			tens_value *= 10;
+		}
+		tens = tens_value / 10;
 
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
-}
+		while (tens >= 1)
+		{
+			unit = positive_value / tens;
+			_putchar(unit + '0');
+			positive_value = (positive_value - (tens * unit));
+			tens /= 10;
+		}
+	}
 
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
 }
