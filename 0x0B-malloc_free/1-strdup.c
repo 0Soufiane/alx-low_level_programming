bff7e27c1e4a3ca8ee9d347cb95979a4b5cfcb01
0Soufiane <ess2003831@gmail.com>
2023-09-05 16:05:42 -0700

    task1.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,35 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-
 /**
- * str_concat - concatenate two string
- *
- * @s1: string
- * @s2: string
- *
- * Return: pointer to newly allocated space or NULL in failure
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
  */
-
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	char *new_str;
-	int i, len, _len;
+	char *aaa;
+	int i, r = 0;
 
-	if (!s1)
-		s1 = "";
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
 
-	if (!s2)
-		s2 = "";
+	aaa = malloc(sizeof(char) * (i + 1));
 
-	len = strlen(s1);
-	_len = strlen(s2);
-
-	new_str = malloc(sizeof(char) * (len + _len + 1));
-
-	if (!new_str)
+	if (aaa == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len + _len; i++)
-		new_str[i] = i < len ? s1[i] : s2[i - len];
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
 
-	return (new_str);
+	return (aaa);
 }
