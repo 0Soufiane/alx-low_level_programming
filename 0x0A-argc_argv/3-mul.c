b0cffd6e9b306a3793542dccc12cfde1a565c3b0
0Soufiane <ess2003831@gmail.com>
2023-09-06 13:24:01 -0700

    checker

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,27 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-  * main - point of entry
-  * Description - A program that multiplies two numbers
-  * @argc: argument counter
-  * @argv: argument vector
-  * Return: always 0 (success)
-  */
+ *main-print sum of 2 numbers.
+ *@argc:number of commandline arguments.
+ *@argv:pointer to an array of commmand line arguments.
+ *Return:0-success, non-zero-fail.
+ */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, j, res;
+	int sum;
 
-	if (argc != 3)
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		i = atoi(argv[1]);
-		j = atoi(argv[2]);
-		res = i * j;
-		printf("%d\n", res);
-	}
+
 	return (0);
 }
