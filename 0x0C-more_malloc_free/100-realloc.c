9a7443f2339599a263cc6b1ab6cedaa1ab512bd0
0Soufiane <ess2003831@gmail.com>
2023-09-07 17:07:49 -0700

    mallocc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,44 +1,42 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * _realloc - a function reallocates a memory block using malloc and free
- * @ptr: a pointer
- * @old_size: old size
- * @new_size: new size
- * Return: pointer
+ * _realloc - reallocates some block of memory to a different block
+ * and frees the old one. If new size < old size, data is truncated
+ *
+ * @ptr: old memory
+ * @old_size: size of old memory
+ * @new_size: new size to allocate
+ *
+ * Return: pointer to new allocation, or 0 on failure
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p, *relloc;
-	unsigned int i;
-
-	if (ptr != NULL)
-	p = ptr;
-
-	else
-	{
-		return (malloc(new_size));
-	}
+	void *ret = 0;
+	char *write, *read;
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0 && ptr != NULL)
+	if (new_size > 0 || ptr == 0)
 	{
-		free(ptr);
-		return (0);
+		ret = malloc(new_size);
+		if (ret == 0)
+			return (0);
 	}
-
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-		return (0);
-
-	for (i = 0; i < (old_size || i < new_size); i++)
+	if (new_size > 0 && ptr != 0)
 	{
-		*(relloc + i) = p[i];
+		write = ret;
+		read = ptr;
+		if (new_size < old_size)
+			old_size = new_size;
+		while (old_size)
+		{
+			old_size--;
+			*write++ = *read++;
+		}
 	}
 
 	free(ptr);
-	return (relloc);
+	return (ret);
 }
