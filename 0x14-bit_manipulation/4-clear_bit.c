31713013a6d445a9f4307c35ef6da5e5077bf0c2
0soufiane <ess2003831@gmail.com>
2023-10-26 13:42:03 -0700

    HardT

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - bit to 0 in an index.
+ *
+ * @n: address of n.
+ * @index:index given 
+ * Return: 1 or ou -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
